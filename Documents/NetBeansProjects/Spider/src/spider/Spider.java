b8f5f20990c1209c9243371cb279141deda2a2f3
borjigin <borjigin@192.168.1.30>
2018-01-13 22:29:47 +0100

    source

---
+++ Documents/NetBeansProjects/Spider/src/spider/Spider.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spider;
+
+/**
+ *
+ * @author borjigin
+ */
+public class Spider {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        goblin gobbo = new goblin();
+        String inhoud = gobbo.raid("https://www.benl.ebay.be/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=warhammer+&_sacat=0");
+        gobbo.scribble("ebaywarhammer.html", inhoud);
+    }
+    
+
+    
+}
