ec705759c5626e84987c4757b2ddf90f3ec37410
borjigin <borjigin@192.168.1.30>
2018-01-14 00:46:18 +0100

    Grimm

--- src/spin/Spin.java
+++ src/spin/Spin.java
@@ -10,7 +10,7 @@ package spin;
  * @author borjigin
  */
 public class Spin {
-    public static final String warh = "https://www.benl.ebay.be/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=warhammer+&_sacat=0";
+    public static final String warh = "https://www.benl.ebay.be/sch/i.html?_odkw=warhammer+-bit&_osacat=0&_ipg=200&_from=R40&_trksid=m570.l1313&_nkw=warhammer&_sacat=0";
     /**
      * @param args the command line arguments
      */
