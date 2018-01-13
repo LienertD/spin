b963bf5753bff1e74ab74e5cb18bba8ff10df3ea
LienertD <lienert.deprez@gmail.com>
2018-01-14 00:46:40 +0100

    Added WIP models

---
+++ src/spin/model/EbayPrice.java
@@ -0,0 +1,24 @@
+package spin.model;
+
+public class EbayPrice {
+
+    private Double price;
+
+    public void setName(Double name) {
+        this.price = name;
+    }
+
+    public Double getName() {
+        return price;
+    }   
+    
+    private DateTime price;
+
+    public void setName(DateTime name) {
+        this.price = name;
+    }
+
+    public Double getName() {
+        return price;
+    }
+}
