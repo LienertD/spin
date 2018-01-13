ea68117fbaeaa15031df0214b815e79a23afcbf9
borjigin <borjigin@192.168.1.30>
2018-01-14 00:12:19 +0100

    sleeprequest

--- src/spin/Spin.java
+++ src/spin/Spin.java
@@ -18,6 +18,7 @@ public class Spin {
         goblin gobbo = new goblin();
         String inhoud = gobbo.raid("https://www.benl.ebay.be/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=warhammer+&_sacat=0");
         gobbo.scribble("ebaywarhammer.html", inhoud);
+        
     }
     
 }
--- src/spin/goblin.java
+++ src/spin/goblin.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,30 +29,19 @@ import org.jsoup.select.Elements;
 public class goblin {
     public final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
     private Document htmlDocument;
+    private Random r ;
+    
+    public goblin(){
+        r=new Random();
+    }
     
     public String raid(String url){
-            try
+        System.out.println("Received web page at " + url); // We were not successful in our HTTP request
+        Elements linksOnPage = htmlDocument.select("a[href]");
+        System.out.println("Found (" + linksOnPage.size() + ") links");
+        for(Element link : linksOnPage)
         {
-            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
-            Document htmlDocument = connection.get();
-            this.htmlDocument = htmlDocument;
-            
-
-            System.out.println("Received web page at " + url);
-            
-            Elements linksOnPage = htmlDocument.select("a[href]");
-            
-            System.out.println("Found (" + linksOnPage.size() + ") links");
-            for(Element link : linksOnPage)
-            {
-                System.out.println(link.attr("href"));
-            }
-            
-        }
-        catch(IOException ioe)
-        {
-            // We were not successful in our HTTP request
-            System.out.println("Error in out HTTP request " + ioe);
+            System.out.println(link.attr("href"));
         }
         return htmlDocument.toString();
     }
@@ -61,6 +51,17 @@ public class goblin {
         List<String> tekst = Arrays.asList(content);
         try {
             Files.write(Paths.get(filename),tekst , Charset.forName("UTF-8"));
+        } catch (IOException ex) {
+            Logger.getLogger(goblin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void request(String url){
+        Thread.sleep(r.);
+        try {
+            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+            Document htmlDocument = connection.get();
+            this.htmlDocument = htmlDocument;
         } catch (IOException ex) {
             Logger.getLogger(goblin.class.getName()).log(Level.SEVERE, null, ex);
         }
