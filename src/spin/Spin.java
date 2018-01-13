440ae7ad887f726cda259907b2ece2d02ad9c2d2
borjigin <borjigin@192.168.1.30>
2018-01-13 23:43:32 +0100

    test

--- src/spin/Spin.java
+++ src/spin/Spin.java
@@ -15,7 +15,9 @@ public class Spin {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        goblin gobbo = new goblin();
+        String inhoud = gobbo.raid("https://www.benl.ebay.be/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=warhammer+&_sacat=0");
+        gobbo.scribble("ebaywarhammer.html", inhoud);
     }
     
 }
